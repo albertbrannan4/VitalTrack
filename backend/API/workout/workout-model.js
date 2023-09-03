c195dd39c886d8d5e42a11bef77728d5bf3e06a9
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-09-03 13:17:52 -0400

    Add and Delete runs

--- backend/API/workout/workout-model.js
+++ backend/API/workout/workout-model.js
@@ -1,20 +1,30 @@
 const db = require("../../data/db-config");
 
-async function getAllWorkouts(user_id) {
+async function getAllRuns(user_id) {
   let result = await db("runs").where("user_id", user_id);
   return result;
 }
 
-async function getWorkoutById(user_id, workout_id) {
+async function getRunById(user_id, run_id) {
   let result = await db("runs")
     .where("user_id", user_id)
-    .where("run_id", workout_id);
+    .where("run_id", run_id);
 
   return result;
 }
 
-async function addWorkout(workout) {
-  let result = await db("runs").insert(workout);
+async function addRun(workout) {
+  let [result] = await db("runs").insert(workout);
+
+  return result;
 }
 
-module.exports = { getAllWorkouts, getWorkoutById };
+async function deleteRun(user_id, run_id) {
+  let result = await db("runs")
+    .del()
+    .where("user_id", user_id)
+    .where("run_id", run_id);
+  return result;
+}
+
+module.exports = { getAllRuns, getRunById, addRun, deleteRun };
--- backend/API/workout/workout-router.js
+++ backend/API/workout/workout-router.js
@@ -11,8 +11,7 @@ router.get("/", restricted, async (req, res) => {
     const userData = jwt.decode(token, JWT_SECRET);
     const { username } = userData;
     const user_id = await User.getIdByUsername(username);
-    console.log(user_id);
-    let allWorkouts = await Workout.getAllWorkouts(user_id);
+    let allWorkouts = await Workout.getAllRuns(user_id);
     res.json(allWorkouts);
   } catch (err) {
     res.status(500).json("error retrieving all workouts");
@@ -26,7 +25,7 @@ router.get("/:id", restricted, async (req, res) => {
     const { username } = userData;
     const user_id = await User.getIdByUsername(username);
 
-    let result = await Workout.getWorkoutById(user_id, req.params.id);
+    let result = await Workout.getRunById(user_id, req.params.id);
     res.json(result);
   } catch (err) {
     res.status(500).json("error retrieving workout");
@@ -34,7 +33,34 @@ router.get("/:id", restricted, async (req, res) => {
 });
 
 router.post("/", restricted, async (req, res) => {
-  res.json("working on adding");
+  const { miles, duration, notes } = req.body;
+  try {
+    const token = req.headers["authorization"];
+    const userData = jwt.decode(token, JWT_SECRET);
+    const user_id = await User.getIdByUsername(userData.username);
+
+    let RunDetails = { user_id, miles, duration, notes: !notes ? "" : notes };
+
+    let response = await Workout.addRun(RunDetails);
+
+    res.json({ message: "Run Added", run_id: response });
+  } catch (err) {
+    res.status(500).json("error adding the workout");
+  }
+});
+
+router.delete("/:id", restricted, async (req, res) => {
+  try {
+    const token = req.headers["authorization"];
+    const userData = jwt.decode(token, JWT_SECRET);
+    const { username } = userData;
+    const user_id = await User.getIdByUsername(username);
+
+    let result = await Workout.deleteRun(user_id, req.params.id);
+    res.json(result);
+  } catch (err) {
+    res.status(500).json("error retrieving workout");
+  }
 });
 
 module.exports = router;
